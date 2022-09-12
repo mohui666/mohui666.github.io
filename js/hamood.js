af6621088a33466f255280f49a2d8845dc8ea4a4
Mo Hui <mmmmmohui@gmail.com>
2022-09-12 17:48:22 +0800

    Update hamood.js

--- js/hamood.js
+++ js/hamood.js
@@ -1,5 +1,10 @@
 const dp = new DPlayer({
     container: document.getElementById('dplayer'),
     video: {
-        url: "../videos/hamood.mp4"
+        url: "../videos/hamood.mp4",
+
+        autoplay:true,
+
+        playbackSpeed:[0.5, 0.75, 1, 1.25, 1.5, 2]
+
     }})
