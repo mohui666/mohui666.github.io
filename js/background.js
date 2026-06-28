e24743e1928a0123a1c81ce8ac29c133836029d0
Mo Hui <68949739+mohui666@users.noreply.github.com>
2026-06-28 23:11:48 +0800

    Rebuild homepage UI and fix background loading

--- js/background.js
+++ js/background.js
@@ -1,21 +1,30 @@
-var bodyBgs = [];    //创建一个数组变量来存储背景图片的路径
+var bodyBgs = [
+    "images/BackGround/bg0.png",
+    "images/BackGround/bg1.png",
+    "images/BackGround/bg2.png",
+    "images/BackGround/bg3.png",
+    "images/BackGround/bg4.png",
+    "images/BackGround/bg5.png",
+    "images/BackGround/bg6.png",
+    "images/BackGround/bg7.png",
+    "images/BackGround/bg8.png",
+    "images/BackGround/bg9.png",
+    "images/BackGround/bg10.png",
+    "images/BackGround/bg11.png",
+    "images/BackGround/bg12.png",
+    "images/BackGround/bg13.png",
+    "images/BackGround/bg14.png"
+];
 
-bodyBgs[0] = "../images/BackGround/bg0.png";
-bodyBgs[1] = "../images/BackGround/bg1.png";
-bodyBgs[2] = "../images/BackGround/bg2.png";
-bodyBgs[3] = "../images/BackGround/bg3.png";
-bodyBgs[4] = "../images/BackGround/bg4.png";
-bodyBgs[5] = "../images/BackGround/bg5.png";
-bodyBgs[6] = "../images/BackGround/bg6.png";
-bodyBgs[7] = "../images/BackGround/bg7.png";
-bodyBgs[8] = "../images/BackGround/bg8.png";
-bodyBgs[9] = "../images/BackGround/bg9.png";
-bodyBgs[10] = "../images/BackGround/bg10.png";
-bodyBgs[11] = "../images/BackGround/bg11.png";
-bodyBgs[12] = "../images/BackGround/bg12.png";
-bodyBgs[13] = "../images/BackGround/bg13.png";
-bodyBgs[14] = "../images/BackGround/bg14.png";
+function applyRandomBackground() {
+    var randomBgIndex = Math.floor(Math.random() * bodyBgs.length);
+    var backgroundUrl = new URL(bodyBgs[randomBgIndex], document.baseURI).href;
 
-var randomBgIndex = Math.round( Math.random() * 14);
+    document.documentElement.style.setProperty("--page-bg-image", "url(\"" + backgroundUrl + "\")");
+}
 
-document.write('<style>body{background:url(' + bodyBgs[randomBgIndex] + '); background-position:center; background-size:cover; background-attachment:fixed;}</style>');
+if (document.readyState === "loading") {
+    document.addEventListener("DOMContentLoaded", applyRandomBackground);
+} else {
+    applyRandomBackground();
+}
